4ad15f457016a0944309775b86080c2f8b7a36b9
soob-forest <dltntnqo@naver.com>
2021-01-19 02:37:55 +0900

    ADD] update service 추가

--- yorijori/src/main/java/org/dnd4/yorijori/domain/posts/service/PostsService.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/posts/service/PostsService.java
@@ -5,7 +5,9 @@ import java.util.stream.Collectors;
 
 import org.dnd4.yorijori.domain.posts.dto.PostsListResDto;
 import org.dnd4.yorijori.domain.posts.dto.PostsResDto;
+import org.dnd4.yorijori.domain.posts.entity.CookingTool;
 import org.dnd4.yorijori.domain.posts.entity.Posts;
+import org.dnd4.yorijori.domain.posts.repository.PostRepository;
 import org.dnd4.yorijori.domain.posts.repository.PostsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,5 +30,19 @@ public class PostsService {
 		return postsRepository.findAll()	
 				.stream().map(posts -> new PostsListResDto(posts))
 				.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public void update(Long id,String title, String subTitle, int likeCount, List<String> imageUrls, List<String> comments, String cookingTime, CookingTool cookingTool){
+
+		Posts post = postsRepository.findById(id).orElseThrow(
+				() -> new IllegalArgumentException("해당 게시물이 없습니다. id = "+id));
+		post.setTitle(title);
+		post.setSubTitle(subTitle);
+		post.setLikeCount(likeCount);
+		post.setImageUrl(imageUrls);
+		post.setComment(comments);
+		post.setCookingTime(cookingTime);
+		post.setCookingTool(cookingTool);
 	}
 }
