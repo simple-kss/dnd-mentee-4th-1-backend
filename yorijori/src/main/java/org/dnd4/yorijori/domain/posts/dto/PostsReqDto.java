86a9cd34e1a1cb17abda0af3e0a86bad3fe18433
soob-forest <dltntnqo@naver.com>
2021-01-19 02:28:51 +0900

    FIX] post req dto constructor 삭제

--- yorijori/src/main/java/org/dnd4/yorijori/domain/posts/dto/PostsReqDto.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/posts/dto/PostsReqDto.java
@@ -1,10 +1,13 @@
 package org.dnd4.yorijori.domain.posts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.dnd4.yorijori.domain.posts.entity.CookingTool;
 import org.dnd4.yorijori.domain.posts.entity.Posts;
 
 import java.util.List;
 
+@Getter @Setter
 public class PostsReqDto {
     private Long id;
     private String title;
@@ -14,15 +17,4 @@ public class PostsReqDto {
     private List<String> comment;
     private String cookingTime;
     private CookingTool cookingTool;
-
-    public PostsReqDto(Posts entity) {
-        this.id=entity.getId();
-        this.title=entity.getTitle();
-        this.subTitle= entity.getSubTitle();
-        this.likeCount= entity.getLikeCount();
-        this.imageUrl= entity.getImageUrl();
-        this.comment= entity.getComment();
-        this.cookingTime= entity.getCookingTime();
-        this.cookingTool= entity.getCookingTool();
-    }
 }
