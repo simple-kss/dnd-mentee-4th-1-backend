97d554d9dfae34d2a0672f27ae2b346683277587
홍지영 <0___0as@naver.com>
2021-02-03 02:28:06 +0900

    [Add] 별점 엔티티 생성

---
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/entity/Rating.java
@@ -0,0 +1,34 @@
+package org.dnd4.yorijori.domain.recipe.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.dnd4.yorijori.domain.user.entity.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Rating {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+    @JoinColumn(name= "user_id", nullable=false)
+    private User user_id;
+	
+	@ManyToOne
+    @JoinColumn(name= "recipe_id", nullable=false)
+    private Recipe recipe_id;
+	
+	private double star;
+}
