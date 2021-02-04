745e0fb0bfa0fe6320514759e925f7064e1a31cf
soob-forest <dltntnqo@naver.com>
2021-02-05 00:17:32 +0900

    [ADD] label entity

---
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/label/entity/Label.java
@@ -0,0 +1,28 @@
+package org.dnd4.yorijori.domain.label.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dnd4.yorijori.domain.common.BaseTimeEntity;
+import org.dnd4.yorijori.domain.recipe.entity.Recipe;
+import org.dnd4.yorijori.domain.user.entity.User;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Label extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
