3f51b8969a1a616c18e1755e6bf929f3bd83c256
soob-forest <dltntnqo@naver.com>
2021-02-05 00:23:17 +0900

    [ADD] Ingredient 엔티티

--- yorijori/src/main/java/org/dnd4/yorijori/domain/ingredient/entity/Ingredient.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/ingredient/entity/Ingredient.java
@@ -4,9 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
