c695927acbff904f6afafeeea1d8a7c6f820bb37
soob-forest <dltntnqo@naver.com>
2021-02-05 00:22:31 +0900

    [ADD] Ingredient 엔티티

---
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/ingredient/entity/Ingredient.java
@@ -0,0 +1,12 @@
+package org.dnd4.yorijori.domain.ingredient.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Ingredient {
+}
