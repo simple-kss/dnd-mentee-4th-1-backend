f8653a3d1b4722e98f4f9ff91e3da79365a021a5
soob-forest <dltntnqo@naver.com>
2021-02-05 00:28:36 +0900

    [ADD] RecipeTheme 엔티티

---
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe_theme/entity/RecipeTheme.java
@@ -0,0 +1,25 @@
+package org.dnd4.yorijori.domain.recipe_theme.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dnd4.yorijori.domain.recipe.entity.Recipe;
+import org.dnd4.yorijori.domain.theme.entity.Theme;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class RecipeTheme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @ManyToOne
+    @JoinColumn(name = "theme_id", nullable = false)
+    private Theme theme;
+}
