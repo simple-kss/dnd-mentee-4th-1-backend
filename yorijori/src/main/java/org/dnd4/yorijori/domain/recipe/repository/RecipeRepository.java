22dcbb8c6410dfd355d0f97c191eb8722ba467b2
soob-forest <dltntnqo@naver.com>
2021-02-06 19:09:37 +0900

    [ADD] recipe repository

---
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/repository/RecipeRepository.java
@@ -0,0 +1,6 @@
+package org.dnd4.yorijori.domain.recipe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeRepository extends JpaRepository<RecipeRepository, Long> {
+}
