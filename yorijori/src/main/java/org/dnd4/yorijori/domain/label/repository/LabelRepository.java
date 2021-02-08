f873a9920af44dfd5beb21b2688b0c8e9d817958
홍지영 <0___0as@naver.com>
2021-02-09 01:26:31 +0900

    [Refact] 코드 정렬

--- yorijori/src/main/java/org/dnd4/yorijori/domain/label/repository/LabelRepository.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/label/repository/LabelRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 public interface LabelRepository extends JpaRepository<Label, Long> {
 
 	@Query(value = "SELECT recipe_id, COUNT(recipe_id)AS coun FROM label WHERE created_date>= :startDate and created_date<= :endDate GROUP BY recipe_id ORDER BY coun DESC LIMIT :limit", nativeQuery = true)
-	List<Long> labelCountDesc(@Param("limit") int limit, @Param("startDate") String startDate, @Param("endDate") String endDate);
-	
+	List<Long> labelCountDesc(@Param("limit") int limit, @Param("startDate") String startDate,
+			@Param("endDate") String endDate);
+
 }
