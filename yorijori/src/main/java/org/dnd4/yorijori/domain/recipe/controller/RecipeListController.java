401db9aa03fa5ff02b906e38534e324906fe28ee
홍지영 <0___0as@naver.com>
2021-02-08 17:51:16 +0900

    [Fix] RecipeListDto에서 ResponseDto로 수정

--- yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/controller/RecipeListController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/controller/RecipeListController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dnd4.yorijori.domain.recipe.dto.RecipeListDto;
+import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
 import org.dnd4.yorijori.domain.recipe.service.RecipeListService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +19,13 @@ public class RecipeListController {
 	private final RecipeListService recipeListService;
 
 	@GetMapping("/recipes")
-	public List<RecipeListDto> recipeList(@RequestParam(required = false) String queryType,
+	public List<ResponseDto> recipeList(@RequestParam(required = false) String queryType,
 			@RequestParam(required = false) String keyword,
 			@RequestParam(required = false, defaultValue = "10") int limit,
 			@RequestParam(required = false, defaultValue = "0") int offset,
 			@RequestParam(required = false) int timeRange,
 			@RequestParam(required = false) int step) {
-		List<RecipeListDto> result = new ArrayList<RecipeListDto>();
+		List<ResponseDto> result = new ArrayList<ResponseDto>();
 		if (queryType == null) {
 			result = recipeListService.findAll(limit, offset);
 		} else if (queryType.equals("search")) {
--- yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/service/RecipeListService.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/service/RecipeListService.java
@@ -3,7 +3,7 @@ package org.dnd4.yorijori.domain.recipe.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.dnd4.yorijori.domain.recipe.dto.RecipeListDto;
+import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
 import org.dnd4.yorijori.domain.recipe.repository.RecipeRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,27 +15,27 @@ import lombok.RequiredArgsConstructor;
 public class RecipeListService {
 	private final RecipeRepository recipeRepository;
 
-	public List<RecipeListDto> findAll(int limit, int offset) {
-		List<RecipeListDto> result = recipeRepository.findAll(PageRequest.of(offset, limit)).stream()
-				.map(RecipeListDto::new).collect(Collectors.toList());
+	public List<ResponseDto> findAll(int limit, int offset) {
+		List<ResponseDto> result = recipeRepository.findAll(PageRequest.of(offset, limit)).stream()
+				.map(ResponseDto::new).collect(Collectors.toList());
 		return result;
 	}
 
-	public List<RecipeListDto> searchRecipes(String keyword, int limit, int offset) {
-		List<RecipeListDto> result = recipeRepository.findByTitleContaining(keyword, limit, offset).stream()
-				.map(RecipeListDto::new).collect(Collectors.toList());
+	public List<ResponseDto> searchRecipes(String keyword, int limit, int offset) {
+		List<ResponseDto> result = recipeRepository.findByTitleContaining(keyword, limit, offset).stream()
+				.map(ResponseDto::new).collect(Collectors.toList());
 		return result;
 	}
 	
-	public List<RecipeListDto> timeRecipes(int timeRange, int limit, int offset){
-		List<RecipeListDto> result = recipeRepository.findByTime(timeRange, limit, offset).stream()
-				.map(RecipeListDto::new).collect(Collectors.toList());
+	public List<ResponseDto> timeRecipes(int timeRange, int limit, int offset){
+		List<ResponseDto> result = recipeRepository.findByTime(timeRange, limit, offset).stream()
+				.map(ResponseDto::new).collect(Collectors.toList());
 		return result;
 	}
 	
-	public List<RecipeListDto> stepRecipes(int step, int limit, int offset){
-		List<RecipeListDto> result = recipeRepository.findByStep(step, limit, offset).stream()
-				.map(RecipeListDto::new).collect(Collectors.toList());
+	public List<ResponseDto> stepRecipes(int step, int limit, int offset){
+		List<ResponseDto> result = recipeRepository.findByStep(step, limit, offset).stream()
+				.map(ResponseDto::new).collect(Collectors.toList());
 		return result;
 	}
 }
