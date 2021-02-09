ceba3ef324ad9f01215aaedac1cc25cf2c408d95
soob-forest <dltntnqo@naver.com>
2021-02-10 00:05:53 +0900

    [FIX] RequestDto step class static으로 변경

--- yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/dto/RequestDto.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/dto/RequestDto.java
@@ -21,7 +21,7 @@ public class RequestDto {
 
     @Embeddable
     @Data
-    public class Step {
+    static public class Step {
         private String image;
         private String description;
     }
