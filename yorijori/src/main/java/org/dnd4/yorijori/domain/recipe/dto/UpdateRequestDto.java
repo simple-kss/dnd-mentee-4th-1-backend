a194d8f1887a466e33e6ce9c2a1dfe9491d7beb8
soob-forest <dltntnqo@naver.com>
2021-02-11 01:15:21 +0900

    [FIX] 필요한 변수들만 추림

--- yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/dto/UpdateRequestDto.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/dto/UpdateRequestDto.java
@@ -1,30 +1,24 @@
 package org.dnd4.yorijori.domain.recipe.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.dnd4.yorijori.domain.ingredient.entity.Ingredient;
-import org.dnd4.yorijori.domain.recipe.entity.Recipe;
-import org.dnd4.yorijori.domain.step.entity.Step;
-import org.dnd4.yorijori.domain.theme.entity.Theme;
-import org.dnd4.yorijori.domain.user.entity.User;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class UpdateRequestDto {
-    private Long id;
     private String title;
     private String thumbnail;
 
-    private List<Ingredient> mainIngredients;
-    private List<Ingredient> subIngredients;
-    private List<Theme> themes;
-    private List<Step> steps;
+    private List<Long> mainIngredientIds;
+    private List<Long> subIngredientIds;
+    private List<Long> themeIds;
+    private List<StepDto> steps;
 
     private int time;
-    private double starCount;
-    private int wishCount;
     private int viewCount;
-    private User writer;
-
 
 }
+
+
