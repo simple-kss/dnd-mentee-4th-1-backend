bfe9b7b988d7c0fa6bca8f69bdc3474849fb8821
홍지영 <0___0as@naver.com>
2021-02-14 15:47:41 +0900

    [Refact] 필요없는거 삭제

--- yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/service/RecipeListService.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/recipe/service/RecipeListService.java
@@ -21,9 +21,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class RecipeListService {
 	
-	private final RecipeRepository recipeRepository;
 	private final RecipeDslRepository recipeDslRepository;
-	private final LabelRepository labelRepository;
 	
 	public List<ResponseDto> findAll(String step, String time, LocalDateTime start, LocalDateTime end, String order, String keyword, int limit, int offset) {
 		return recipeDslRepository.findAll(step, time, start, end, order, keyword, limit, offset).stream()
@@ -36,65 +34,4 @@ public class RecipeListService {
 		return list;
 	}
 	
-	public List<ResponseDto> findAll(int limit, int offset, String order, LocalDateTime startDate,
-			LocalDateTime endDate, int timeRange) {
-		List<ResponseDto> result = new ArrayList<ResponseDto>();
-		if (order.equals("view")) {
-			Sort sort = Sort.by(Sort.Direction.DESC, "viewCount");
-			Pageable pageable = PageRequest.of(offset, limit, sort);
-			result = recipeRepository.findByTimeLessThanEqualAndCreatedDateBetween(timeRange, startDate, endDate, pageable).stream()
-					.map(ResponseDto::new).collect(Collectors.toList());
-		}
-		if (order.equals("latest")) {
-			Sort sort = Sort.by(Sort.Direction.DESC, "id");
-			Pageable pageable = PageRequest.of(offset, limit, sort);
-			result = recipeRepository.findByTimeLessThanEqualAndCreatedDateBetween(timeRange, startDate, endDate, pageable).stream()
-					.map(ResponseDto::new).collect(Collectors.toList());
-		}
-		return result;
-	}
-
-	public List<ResponseDto> searchRecipes(String keyword, int limit, int offset, String order) {
-		List<ResponseDto> result = new ArrayList<ResponseDto>();
-		if (order.equals("view")) {
-			Sort sort = Sort.by(Sort.Direction.DESC, "viewCount");
-			Pageable pageable = PageRequest.of(offset, limit, sort);
-			result = recipeRepository.findBytitleContaining(keyword, pageable).stream().map(ResponseDto::new)
-					.collect(Collectors.toList());
-		}
-		if (order.equals("latest")) {
-			Sort sort = Sort.by(Sort.Direction.DESC, "id");
-			Pageable pageable = PageRequest.of(offset, limit, sort);
-			result = recipeRepository.findBytitleContaining(keyword, pageable).stream().map(ResponseDto::new)
-					.collect(Collectors.toList());
-		}
-		return result;
-	}
-
-	public List<ResponseDto> timeRecipes(int timeRange, int limit, int offset) {
-		List<ResponseDto> result = recipeRepository.findByTime(timeRange, limit, offset).stream().map(ResponseDto::new)
-				.collect(Collectors.toList());
-		return result;
-	}
-
-	public List<ResponseDto> stepRecipes(int step, int limit, int offset) {
-		List<ResponseDto> result = recipeRepository.findByStep(step, limit, offset).stream().map(ResponseDto::new)
-				.collect(Collectors.toList());
-		return result;
-	}
-
-	public List<ResponseDto> labelCountDesc(int limit, String startDate, String endDate) {
-		
-		
-		List<Long> recipe_id = labelRepository.labelCountDesc(limit, startDate, endDate);
-		List<Recipe> entity = new ArrayList<Recipe>();
-
-		for (Long id : recipe_id) {
-			Recipe recipe = recipeRepository.getOne(id);
-			entity.add(recipe);
-		}
-		List<ResponseDto> result = entity.stream().map(ResponseDto::new).collect(Collectors.toList());
-
-		return result;
-	}
 }
