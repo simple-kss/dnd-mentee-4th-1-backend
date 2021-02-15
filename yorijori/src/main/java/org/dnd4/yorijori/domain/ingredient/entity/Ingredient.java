5554e6e09f25fde3cc339c6a7c1cd1c1b10ce60f
soob-forest <dltntnqo@naver.com>
2021-02-15 22:42:30 +0900

    [FIX] Ingredient 엔티티 unit, quantity, recipe 추가

--- yorijori/src/main/java/org/dnd4/yorijori/domain/ingredient/entity/Ingredient.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/ingredient/entity/Ingredient.java
@@ -3,6 +3,7 @@ package org.dnd4.yorijori.domain.ingredient.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.dnd4.yorijori.domain.recipe.entity.Recipe;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,9 +18,17 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Recipe recipe;
     private String name;
+    private String unit;
+    private String quantity;
     @Builder
-    public Ingredient(String name){
+    public Ingredient(String name, String unit, String quantity){
         this.name = name;
+        this.unit = unit;
+        this.quantity = quantity;
+    }
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
     }
 }
