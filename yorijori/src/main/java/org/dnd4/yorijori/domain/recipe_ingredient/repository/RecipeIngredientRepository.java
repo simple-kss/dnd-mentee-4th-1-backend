ab03d8d72a813365ca86a1ce179b4175d6e790f3
soob-forest <dltntnqo@naver.com>
2021-02-15 22:43:06 +0900

    [FIX] recipeIngredient 삭제

--- yorijori/src/main/java/org/dnd4/yorijori/domain/recipe_ingredient/repository/RecipeIngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dnd4.yorijori.domain.recipe_ingredient.repository;
-
-import org.dnd4.yorijori.domain.recipe_ingredient.entity.RecipeIngredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
-}
