d6b7d405fdf14bbdc76bd7f37dd502ce2a51d00a
홍지영 <0___0as@naver.com>
2021-02-17 20:28:20 +0900

    [Add] 사용자 확인

---
+++ yorijori/src/main/java/org/dnd4/yorijori/Security/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package org.dnd4.yorijori.Security;
+
+import org.dnd4.yorijori.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
