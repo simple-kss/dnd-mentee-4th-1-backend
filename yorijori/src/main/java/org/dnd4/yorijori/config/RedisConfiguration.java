07f5514522d7f574b549e89856135c0b72e95f8f
홍지영 <0___0as@naver.com>
2021-02-18 23:00:17 +0900

    [Fix] 레디스 롱타입 잘 나오게

--- yorijori/src/main/java/org/dnd4/yorijori/config/RedisConfiguration.java
+++ yorijori/src/main/java/org/dnd4/yorijori/config/RedisConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -20,6 +21,7 @@ public class RedisConfiguration {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<Long>(Long.class));
         return redisTemplate;
     }
  
