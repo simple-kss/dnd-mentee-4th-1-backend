64f03108149523437dc282091709b1b8dec9c421
홍지영 <0___0as@naver.com>
2021-02-18 21:06:12 +0900

    [Fix] sorted set으로 변경

--- yorijori/src/main/java/org/dnd4/yorijori/domain/monthly_view/controller/MonthlyViewController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/monthly_view/controller/MonthlyViewController.java
@@ -1,35 +1,43 @@
 package org.dnd4.yorijori.domain.monthly_view.controller;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
-import org.dnd4.yorijori.domain.monthly_view.entity.MonthlyView;
-import org.dnd4.yorijori.domain.monthly_view.repository.MonthlyViewRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
+import org.dnd4.yorijori.domain.recipe.entity.Recipe;
+import org.dnd4.yorijori.domain.recipe.service.RecipeService;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/view")
 public class MonthlyViewController {
 
-	@Autowired
-	private MonthlyViewRepository monthlyViewRepository;
+	@Resource(name = "redisTemplate")
+	private ZSetOperations<String, Long> zSetOperations;
+	private final RecipeService recipeService;
 	
-	@GetMapping
+	@PostMapping
 	public void increase(@RequestParam(value = "recipe") Long recipe_id) {
-		Optional<MonthlyView> optional = monthlyViewRepository.findByrecipeId(recipe_id);
-		if(optional.isPresent()) {
-			MonthlyView entity = optional.get();
-			entity.increase();
-			monthlyViewRepository.save(entity);
-		} else {
-			MonthlyView entity = new MonthlyView();
-			entity.setRecipeId(recipe_id);
-			entity.setCount(0);
-			monthlyViewRepository.save(entity);
-		}
+		zSetOperations.incrementScore("view", recipe_id, 1);
 	}
-	
+
+	@GetMapping("/rank")
+	public List<Recipe> viewTop(@RequestParam(value = "top") int top) {
+		List<Recipe> result = new ArrayList<>();
+		Set<Long> recipes = zSetOperations.reverseRange("view", 0, top - 1);
+		for (Long rid : recipes) {
+			result.add(recipeService.get(rid));
+		}
+		return result;
+	}
 }
