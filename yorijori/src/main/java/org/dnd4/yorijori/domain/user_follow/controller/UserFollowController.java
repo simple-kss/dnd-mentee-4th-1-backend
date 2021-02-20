6bafe81054873edbe0986f0b814546e640506919
홍지영 <0___0as@naver.com>
2021-02-21 00:37:50 +0900

    [Refact] 코드 정렬

--- yorijori/src/main/java/org/dnd4/yorijori/domain/user_follow/controller/UserFollowController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/user_follow/controller/UserFollowController.java
@@ -1,11 +1,9 @@
 package org.dnd4.yorijori.domain.user_follow.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
 import org.dnd4.yorijori.domain.recipe.dto.UserDto;
-import org.dnd4.yorijori.domain.recipe.repository.RecipeRepository;
 import org.dnd4.yorijori.domain.user.entity.User;
 import org.dnd4.yorijori.domain.user.repository.UserRepository;
 import org.dnd4.yorijori.domain.user_follow.service.UserFollowService;
@@ -49,15 +47,19 @@ public class UserFollowController {
 	}
 
 	@GetMapping("/user/{userId}/followerFeed")
-	public List<ResponseDto> followerFeed(@PathVariable Long userId, @RequestParam(required = false, defaultValue = "10") int limit, @RequestParam(required = false, defaultValue = "0") int offset) {
+	public List<ResponseDto> followerFeed(@PathVariable Long userId,
+			@RequestParam(required = false, defaultValue = "10") int limit,
+			@RequestParam(required = false, defaultValue = "0") int offset) {
 		List<ResponseDto> result = userFollowService.followerFeed(userId, limit, offset);
 		return result;
 	}
-	
+
 	@GetMapping("/user/{userId}/followingFeed")
-	public List<ResponseDto> followingFeed(@PathVariable Long userId, @RequestParam(required = false, defaultValue = "10") int limit, @RequestParam(required = false, defaultValue = "0") int offset) {
+	public List<ResponseDto> followingFeed(@PathVariable Long userId,
+			@RequestParam(required = false, defaultValue = "10") int limit,
+			@RequestParam(required = false, defaultValue = "0") int offset) {
 		List<ResponseDto> result = userFollowService.followingFeed(userId, limit, offset);
 		return result;
 	}
-	
+
 }
