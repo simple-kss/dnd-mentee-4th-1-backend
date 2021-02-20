d9fd062b504f02f80d19203be8ae1af4c6e99e16
홍지영 <0___0as@naver.com>
2021-02-20 23:59:34 +0900

    [Add] 팔로우 컨트롤러 추가

--- yorijori/src/main/java/org/dnd4/yorijori/domain/user_follow/controller/UserFollowController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/user_follow/controller/UserFollowController.java
@@ -1,5 +1,48 @@
 package org.dnd4.yorijori.domain.user_follow.controller;
 
+import java.util.List;
+
+import org.dnd4.yorijori.domain.recipe.dto.UserDto;
+import org.dnd4.yorijori.domain.user.entity.User;
+import org.dnd4.yorijori.domain.user.repository.UserRepository;
+import org.dnd4.yorijori.domain.user_follow.service.UserFollowService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
 public class UserFollowController {
+
+	private final UserFollowService userFollowService;
+	private final UserRepository userRepository;
+
+	@GetMapping("/user/{userId}/follower")
+	public List<UserDto> followerList(@PathVariable Long userId) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + userId));
+		return userFollowService.followerList(user);
+	}
+
+	@GetMapping("/user/{userId}/following")
+	public List<UserDto> followingList(@PathVariable Long userId) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + userId));
+		return userFollowService.followingList(user);
+	}
+
+	@PostMapping("/user/{following}/follow")
+	public void follow(@PathVariable Long followingId, @RequestParam Long followerId) {
+		userFollowService.follow(followingId, followerId);
+	}
+
+	@PostMapping("/user/{following}/unfollow")
+	public void unfollow(@PathVariable Long followingId, @RequestParam Long followerId) {
+		userFollowService.unfollow(followingId, followerId);
+	}
 
 }
--- yorijori/src/main/java/org/dnd4/yorijori/domain/user_follow/service/UserFollowService.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/user_follow/service/UserFollowService.java
@@ -1,10 +1,12 @@
 package org.dnd4.yorijori.domain.user_follow.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.dnd4.yorijori.domain.recipe.dto.UserDto;
 import org.dnd4.yorijori.domain.user.entity.User;
+import org.dnd4.yorijori.domain.user.repository.UserRepository;
 import org.dnd4.yorijori.domain.user_follow.entity.UserFollow;
 import org.dnd4.yorijori.domain.user_follow.repository.UserFollowRepository;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class UserFollowService {
 	
 	private final UserFollowRepository userFollowRepository;
+	private final UserRepository userRepository;
 
 	public List<UserDto> followingList(User user) {
 		List<UserDto> result = userFollowRepository.findfollowingByfollower(user).stream()
@@ -30,13 +33,17 @@ public class UserFollowService {
 	}
 	
 	@Transactional
-    public void follow(User following, User follower){
+    public void follow(Long followingId, Long followerId){
+		User follower = userRepository.findById(followerId).orElseThrow(()->new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + followerId));
+		User following = userRepository.findById(followingId).orElseThrow(()->new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + followingId));
 		UserFollow entity = UserFollow.builder().follower(follower).following(following).build();
 		userFollowRepository.save(entity);
     }
 	
 	@Transactional
-    public void unfollow(User following, User follower){
+    public void unfollow(Long followingId, Long followerId){
+		User follower = userRepository.findById(followerId).orElseThrow(()->new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + followerId));
+		User following = userRepository.findById(followingId).orElseThrow(()->new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + followingId));
 		UserFollow entity = userFollowRepository.findByFollowerAndFollowing(follower, following).orElseThrow(()->new IllegalArgumentException("데이터가 없습니다."));
 		userFollowRepository.delete(entity);
     }
